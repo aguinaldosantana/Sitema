b488d18473f364375c382a0c5943295070ef179c
Agsantana <aguinaldosantana6@gmail.com>
2023-04-13 23:17:18 -0300

    primeiro commit

--- SitemaTEY/Controllers/WeatherForecastController.cs
+++ SitemaTEY/Controllers/WeatherForecastController.cs
@@ -23,6 +23,7 @@ namespace SitemaTEY.Controllers
         {
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
+
                 Date = DateTime.Now.AddDays(index),
                 TemperatureC = Random.Shared.Next(-20, 55),
                 Summary = Summaries[Random.Shared.Next(Summaries.Length)]
